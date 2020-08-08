95c015570f6aed9e655875a8585b4ec17f840471
zagizik <63964521+zagizik@users.noreply.github.com>
2020-08-08 17:38:46 +0300

    UserDefaults, Codable, NSCoding

---
+++ Project12b/Project10/ViewController.swift
@@ -0,0 +1,124 @@
+//
+//  ViewController.swift
+//  Project10
+//
+//  Created by Александр Банников on 05.08.2020.
+//  Copyright © 2020 AlexBanana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    var people = [Person]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let defaults = UserDefaults.standard
+
+        if let savedPeople = defaults.object(forKey: "people") as? Data {
+            let jsonDecoder = JSONDecoder()
+
+            do {
+                people = try jsonDecoder.decode([Person].self, from: savedPeople)
+            } catch {
+                print("Failed to load people")
+            }
+        }
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return people.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as! PersonCell
+
+        let person = people[indexPath.item]
+
+        cell.name.text = person.name
+
+        let path = getDocumentsDirectory().appendingPathComponent(person.image)
+        cell.imageView.image = UIImage(contentsOfFile: path.path)
+
+        cell.imageView.layer.borderColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.3).cgColor
+        cell.imageView.layer.borderWidth = 2
+        cell.imageView.layer.cornerRadius = 3
+        cell.layer.cornerRadius = 7
+
+        return cell
+    }
+
+    @objc func addNewPerson() {
+        let picker = UIImagePickerController()
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.sourceType = .camera
+        }
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.originalImage] as? UIImage else { return }
+
+        let imageName = UUID().uuidString
+        let imagePath = getDocumentsDirectory().appendingPathComponent(imageName)
+
+        if let jpegData = image.jpegData(compressionQuality: 0.8) {
+            try? jpegData.write(to: imagePath)
+        }
+
+        let person = Person(name: "Unknown", image: imageName)
+        people.append(person)
+
+        collectionView?.reloadData()
+
+        dismiss(animated: true)
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let person = people[indexPath.item]
+
+        let ac = UIAlertController(title: "Select action", message: nil, preferredStyle: .alert)
+        
+
+        ac.addAction(UIAlertAction(title: "Delete", style: .destructive){
+            [unowned self] _ in
+            self.people.remove(at: indexPath.item)
+            collectionView.reloadData()
+        })
+
+        ac.addAction(UIAlertAction(title: "Rename", style: .default) { [unowned self] _ in
+            let secAC = UIAlertController(title: "Type new name", message: nil, preferredStyle: .alert)
+            secAC.addTextField()
+            secAC.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+            secAC.addAction(UIAlertAction(title: "Rename", style: .default) { [unowned self] _ in
+                let newName = secAC.textFields![0]
+                person.name = newName.text!
+
+                self.collectionView?.reloadData()
+                
+            })
+            self.present(secAC, animated: true)
+        })
+
+        present(ac, animated: true)
+    }
+
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentsDirectory = paths[0]
+        return documentsDirectory
+    }
+    
+    func save() {
+        let jsonEncoder = JSONEncoder()
+        if let savedData = try? jsonEncoder.encode(people) {
+            let defaults = UserDefaults.standard
+            defaults.set(savedData, forKey: "people")
+        } else {
+            print("Failed to save people.")
+        }
+    }
+}
