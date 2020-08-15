61e2e5a492b4de96f3460cb0815acb5b00be386f
zagizik <63964521+zagizik@users.noreply.github.com>
2020-08-15 22:51:15 +0300

    Extension, JavaScript Ejection

---
+++ Project19/Project19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project19
+//
+//  Created by Александр Банников on 15.08.2020.
+//  Copyright © 2020 AlexBanana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
